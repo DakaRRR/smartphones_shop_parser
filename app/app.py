6e04c41aa936ddaba9b666da9e7ebefca4c1213d
DakaR <kanat.aidahar@gmail.com>
2023-01-25 14:28:58 +0600

    измение пути файла, smartphone.json

--- app/app.py
+++ app/app.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI, Query
 import json
+import os
+
 
 fastapi_app = FastAPI()
 
 
 def get_smartphones_json(filepath: str):
     """Читаем данные с файла smartphones json"""
-    with open(filepath, "r", encoding='utf-8') as file:
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    parser_path = os.path.join(base_path, '..', 'parser')
+    full_path = os.path.join(parser_path, filepath)
+    with open(full_path, "r", encoding='utf-8') as file:
         smartphones = json.load(file)
     return smartphones
 
@@ -14,6 +19,6 @@ def get_smartphones_json(filepath: str):
 @fastapi_app.get("/smartphones")
 def get_smartphones_by_price(price: int = Query(..., gt=0)):
     """Эндпоинт по поиску смартфона по цене"""
-    smartphones = get_smartphones_json("parser/smartphones.json")
+    smartphones = get_smartphones_json("smartphones.json")
     smartphones_filter_by_price = [s for s in smartphones if s["price"] == price]
     return smartphones_filter_by_price
